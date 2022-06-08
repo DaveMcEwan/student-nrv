f9e8375e05300be1d967afc48c07abdba56905e1
nesi <nelson.silva@nordicsemi.no>
2022-06-08 15:18:02 +0200

    Experimenting with moving dictionaries out of the main script; currently functional but not automated yet so that it is based on the extensions

---
+++ isa/base_loads.py
@@ -0,0 +1,10 @@
+all_dicts = {}
+
+base_loads_dict = {  'lw': {'bytes': 4, 'cycles': 0}, 
+                'lh': {'bytes': 2, 'cycles': 0},
+                'lhu': {'bytes': 2, 'cycles': 0},
+                'lb': {'bytes': 1, 'cycles': 0},
+                'lbu': {'bytes': 1, 'cycles': 0},
+             }
+
+all_dicts.update(base_loads_dict)
