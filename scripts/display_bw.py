2d58cd6f83a6da6fd6a343d45e7812448c326ffc
nesi <nelson.silva@nordicsemi.no>
2022-06-13 17:02:55 +0200

    Added function that takes in the list provided by the moving_average() function and produces a line graph using matplotlib

--- scripts/display_bw.py
+++ scripts/display_bw.py
@@ -1,23 +1,28 @@
 import sys
-
-# Need a list to store the previous n values
-# There are no fixed length python lists
+import matplotlib.pyplot as plt
 
 def moving_average(window_size):
+    avg_stream = []
     window = []
     for bytes in map(str.rstrip, sys.stdin):
+        avg_stream.append(int(bytes))
         window.append(int(bytes))
         if (len(window)) > window_size:
             window.pop(0)
         print(sum(window)/len(window))
+    return avg_stream
 
-# def display_graph():
-    #
+def display_graph(avg_stream):
+    time_axis = list(range(len(avg_stream)))
+    plt.plot(time_axis, avg_stream)
+    plt.title('Moving Average output')
+    plt.xlabel('Line Number')
+    plt.ylabel('Bytes transferred')
+    plt.savefig("test.png")
 
 def main():
     n = 4
-    moving_average(n)
-    # display_graph()
+    display_graph(moving_average(n))
 
 if __name__ == "__main__":
     main()
