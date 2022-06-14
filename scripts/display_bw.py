7e5ee52f74c55453930e57a3a2f3288a5414f51f
nesi <nelson.silva@nordicsemi.no>
2022-06-14 16:00:18 +0200

    Updated display script to vary the moving average window size through squared values of 2 and make multiple pngs

--- scripts/display_bw.py
+++ scripts/display_bw.py
@@ -6,10 +6,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--img", help="Save path for the png figure")
 args = parser.parse_args() # File path stored in args.img
 
-def moving_average(window_size):
+def moving_average(input_stream, window_size):
     avg_stream = []
     window = []
-    for bytes in map(str.rstrip, sys.stdin):
+    for bytes in input_stream:
+        # print("Bytes:"+bytes)
         window.append(int(bytes))
         if (len(window)) > window_size:
             window.pop(0)
@@ -18,17 +19,26 @@ def moving_average(window_size):
         print(averaged_value)
     return avg_stream
 
-def display_graph(avg_stream):
+def display_graph(avg_stream, n):
     time_axis = list(range(len(avg_stream)))
+
+    plt.figure(n)
     plt.plot(time_axis, avg_stream)
-    plt.title('Moving Average output')
+    plt.title('Moving Average output for n='+str(n))
     plt.xlabel('Line Number')
     plt.ylabel('Bytes transferred')
-    plt.savefig(args.img)
+    plt.savefig(args.img+"_"+str(n)+".png")
 
 def main():
-    n = 128
-    display_graph(moving_average(n))
+    # Read the stdin input as a variable so we can then return to it
+    # input = map(str.rstrip, sys.stdin.readlines())
+    input = sys.stdin.readlines()
+    print(input)
+
+    for n in range(10):
+        n = 1 << n # Square the 2 to adjust the window size
+        print("Window Size: "+str(n))
+        display_graph(moving_average(input, n), n)
 
 if __name__ == "__main__":
     main()
