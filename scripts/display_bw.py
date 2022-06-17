646bff69f90b68407e14425eee0687ad71cc7ef5
nesi <nelson.silva@nordicsemi.no>
2022-06-17 13:04:40 +0200

    Adjusting display_bw.py because there was a fault in my moving average function. Comparing convolution functions provided by numpy and scipy; will go forward with the scipy convolution function after measuring it to be about 30x faster

--- scripts/display_bw.py
+++ scripts/display_bw.py
@@ -1,23 +1,24 @@
+# Script used to calculate the moving average of a byte stream and plot the 
+#   
+
 import sys
 import argparse
 import matplotlib.pyplot as plt
+
+import time
+import numpy as np
+import scipy
+from scipy.ndimage.filters import uniform_filter1d
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--img", help="Save path for the png figure")
 args = parser.parse_args() # File path stored in args.img
 
-def moving_average(input_stream, window_size):
-    avg_stream = []
-    window = []
-    for bytes in input_stream:
-        # print("Bytes:"+bytes)
-        window.append(int(bytes))
-        if (len(window)) > window_size:
-            window.pop(0)
-        averaged_value = sum(window)/len(window)
-        avg_stream.append(averaged_value)
-        print(averaged_value)
-    return avg_stream
+def moving_average_scipy(input_stream, window_size):
+    return list(scipy.convolve(input_stream, np.ones(window_size), 'same') / window_size)
+
+def moving_average_numpy(input_stream, window_size):
+    return list(np.convolve(input_stream, np.ones(window_size), 'same') / window_size)
 
 def display_graph(avg_stream, n):
     time_axis = list(range(len(avg_stream)))
@@ -33,12 +34,31 @@ def main():
     # Read the stdin input as a variable so we can then return to it
     # input = map(str.rstrip, sys.stdin.readlines())
     input = sys.stdin.readlines()
-    print(input)
+    input_floats = [float(x) for x in input]
 
-    for n in range(10):
-        n = 1 << n # Square the 2 to adjust the window size
-        print("Window Size: "+str(n))
-        display_graph(moving_average(input, n), n)
+    start_numpy = time.time()
+    print("Numpy convolve output:")
+    print(moving_average_numpy(input_floats, 4))
+
+    end = time.time()
+    print("Scipy convolve output:")
+    print(moving_average_scipy(input_floats, 4))
+    end_scipy = time.time()
+
+    print("Time taken to complete Numpy convolution: "+str(end-start_numpy))
+    print("Time taken to complete Scipy convolution: "+str(end_scipy-end))
+
+    # for n in range(10):
+    #     n = 1 << n # Square the 2 to adjust the window size
+    #     print("Window Size: "+str(n))
+
+    #     print("Numpy Convolve : ")
+    #     print(moving_average_numpy(input_floats, n))
+    #     print("My Convolve: ")
+    #     print(moving_average(input, n))
+    #     print()
+
+        # display_graph(moving_average(input, n), n)
 
 if __name__ == "__main__":
     main()
