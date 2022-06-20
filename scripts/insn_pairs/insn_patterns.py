b604c257b3448944b5e8a71f548b8e38e51d08be
nesi <nelson.silva@nordicsemi.no>
2022-06-20 13:28:13 +0200

    Filtering and sorting added to insn_patterns.py to prepare it for differentation calculations in order to detect local maxima

--- scripts/insn_pairs/insn_patterns.py
+++ scripts/insn_pairs/insn_patterns.py
@@ -14,7 +14,7 @@ import sys
 #   Iterate through the instruction stream and calculate the most frequent
 #       instruction patterns of size n
 def track_patterns(instr_trace, n):
-    pairs_dict = {}
+    patterns_dict = {}
     window = [0]*n # Empty list to represent the moving window looking for patterns
 
     # Set up the first window
@@ -22,7 +22,7 @@ def track_patterns(instr_trace, n):
         window[i] = instr_trace[i].split()[1]
     
     # previous_instr = instr_trace[0].split()[1] # Set the first instruction first 
-    pairs_dict[tuple(window)] = 1
+    patterns_dict[tuple(window)] = 1
 
     # Iterate through the rest of the instructions
     for line in instr_trace[n:]:
@@ -33,15 +33,15 @@ def track_patterns(instr_trace, n):
         window.append(insn_name)
         window.pop(0)
 
-        if (pairs_dict.get(tuple(window), 0) == 0):
-            pairs_dict[tuple(window)] = 1
+        if (patterns_dict.get(tuple(window), 0) == 0):
+            patterns_dict[tuple(window)] = 1
         else:
-            pairs_dict[tuple(window)] += 1
+            patterns_dict[tuple(window)] += 1
     
     # Sort based on the corresponding counter values
-    sorted_pairs = sorted(pairs_dict.items(), key=lambda x: x[1], reverse=True)
+    # sorted_pairs = sorted(patterns_dict.items(), key=lambda x: x[1], reverse=True)
 
-    return sorted_pairs
+    return patterns_dict
     # Returns a list of tuples where each instruction pattern is associated with their counter
 
 # Takes in the list of tuples and prints out the pairs and counters in a readable way
@@ -52,11 +52,22 @@ def print_pairs(sorted_pairs):
             print_template += pair[0][insn] + ", "
         print(f'{print_template:<64} {str(pair[1])}')
 
-def main():
 
+def main():
     # Read in the stdin and store in the instr_trace variable
     instr_trace = sys.stdin.readlines()
-    print_pairs(track_patterns(instr_trace, 7))
+    all_patterns_dict = {}
+
+    for i in range(3, 8):
+        all_patterns_dict.update(track_patterns(instr_trace, i))
+
+    # Only printing the top third most frequent patterns to filter out the many patterns with a counter of 1
+
+    # print(all_patterns_dict.items())
+    # List comprehension to filter out patterns smaller than a certain counter value
+    filtered_patterns = {k:r for k, r in all_patterns_dict.items() if r > 5}
+    sorted_patterns = sorted(filtered_patterns.items(), key=lambda x: x[1], reverse=True)
+    print_pairs(sorted_patterns)
 
 if __name__ == "__main__":
     main()
