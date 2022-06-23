bba560c0b01577dd37f0f5b9c3c01c90a6c09593
nesi <nelson.silva@nordicsemi.no>
2022-06-23 11:27:21 +0200

    Completed optimisations leading to speedups of 23%, 59% and 31% in pattern frequency calculations, maxima calculations and formatted pattern printing respectively. Optimisations made will be detailed in the following comment on the PR

--- scripts/insn_pairs/insn_patterns.py
+++ scripts/insn_pairs/insn_patterns.py
@@ -44,7 +44,6 @@ def track_patterns(instr_trace, n):
     #   their counter   
     return patterns_dict
 
-
 # Function that takes in the dictionary of all patterns and locates the local maxima
 #   by filtering patterns that don't occur frequently, sorting the dictionary
 #   and then differentiating with respect to 1. Any differentiated values above 
@@ -61,52 +60,37 @@ def local_maxima(all_patterns_dict, min, diff_threshold, plot):
     # Differentiate the sorted dictionary values
     dy = np.append(abs(diff(list(sorted_patterns.values()))/1), 0)
 
-    # Form a mask of True and Falses indicating where the peak value of that region is
-    #   Also popping the end of the list to shift the values forward so that
-    #   the 'True's correspond with the maximum value in a section
-    maximum_indices = np.array([True if x > diff_threshold else False for x in dy][:-1])
+    # Initialise with the largest value in the patterns list
+    maxima = [sorted_patterns_list[0]]
+    # If the current value is above the threshold, add the next index to the return list
+    for index, x in enumerate(dy):
+        if x > diff_threshold and index < len(dy)-1: # Append next index in patterns list
+            maxima.append(sorted_patterns_list[index+1])
 
-    # Insert True at the very start since the pattern that appeared the most
-    #   is definitely part of the list
+    if (plot):
+        visualise_indices(sorted_patterns, dy, diff_threshold)
+
+    return maxima
+
+def visualise_indices(sorted_patterns, dy, diff_threshold):
+    maximum_indices = np.array([True if x > diff_threshold else False for x in dy][:-1])
     maximum_indices = np.insert(maximum_indices, 0, True)
 
-    return_list =[]
-    for index in enumerate(maximum_indices):
-        if (index[1]):
-            return_list.append(sorted_patterns_list[index[0]])
+    # Plot the sorted list
+    plt.bar(range(len(sorted_patterns)), list(dict(sorted_patterns).values()))
 
-    return return_list
-    # Grab the instruction tuples which the indices align with
-    masked_values = np.array(list(sorted_patterns))[maximum_indices]
-
-    # Optional bool to plot to therefore allow a user to visualise which local
-    #   maxima was selected - allows the user to verify that the local maxima was selected
-    if (plot):
-        # Plot the sorted list
-        plt.bar(range(len(sorted_patterns)), list(dict(sorted_patterns).values()))
-
-        # Also plot these indexes on the same graph to see how the values align to
-        #   the instruction patterns
-        plt.bar(range(len(sorted_patterns)), maximum_indices)
-        plt.show()
-
-    # Return the dictionary values (frequency counters) 
-    return ({key:filtered_patterns.get(key) for key in masked_values}).items()
-
-# Takes in a dictionary and prints out the pairs and counters in a readable way
-# def print_pairs(sorted_pairs):
-#     for k, v in sorted_pairs.items():
-#         # Remove brackets and apostrophes
-#         stripped_key = str(k)[1:-1].replace("'","")
-#         # Print in a formaatted manner
-#         print(f'{stripped_key:<64} {str(v)}')
+    # Also plot these indexes on the same graph to see how the values align to
+    #   the instruction patterns
+    plt.bar(range(len(sorted_patterns)), maximum_indices)
+    plt.show()
 
 def print_pairs(sorted_pairs):
     for pair in sorted_pairs:
         stripped_key = str(pair[0])[1:-1].replace("'","")
         print(f'{f"{stripped_key}":<32} {str(pair[1])}')
 
-def main():
+# Main function with timing in case I want to come back and optimise again
+def timed_main():    
     # Read in the stdin and store in the instr_trace variable
     instr_trace = sys.stdin.readlines()
     all_patterns_dict = {}
@@ -126,6 +110,17 @@ def main():
     print_pairs(maxima)
     end_time = time.time()
     print("Time taken to print = "+str(end_time-start_time))
+
+# Default main
+def main():
+    # Read in the stdin and store in the instr_trace variable
+    instr_trace = sys.stdin.readlines()
+    all_patterns_dict = {}
+
+    for i in range(3, 8):
+        all_patterns_dict.update(track_patterns(instr_trace, i))
+
+    print_pairs(local_maxima(all_patterns_dict, 5, 10, False))
 
 if __name__ == "__main__":
     main()
