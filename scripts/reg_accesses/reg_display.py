eb5630dfadb2b0813d012f7415924087c928f7a1
nesi <nelson.silva@nordicsemi.no>
2022-08-02 12:11:18 +0200

    Updating to have an output png filepath flag on the script and making changes so that it matches the changes made to reg_accesses.py (removing immediate display code and adjusting the JSON file parsing)

--- scripts/reg_accesses/reg_display.py
+++ scripts/reg_accesses/reg_display.py
@@ -4,6 +4,15 @@
 # Output : Several figures plotted using Matplotlib visualising the distribution
 #   per register type.
 
+# Example to guide use:
+# Run the command : python3 scripts/reg_accesses/reg_display.py \
+#   --isa=rv32ic -j=<input json file from reg_accesses.py> \
+#   --img=<output png prefix>
+#   < scripts/example.trc
+#   while in the base directory
+
+# Produces multiple figures, each with their own suffix
+
 import argparse
 import json
 import matplotlib.pyplot as plt
@@ -11,6 +20,7 @@ import matplotlib.pyplot as plt
 # Input argument parsing (to detect the input filepath)
 parser = argparse.ArgumentParser()
 parser.add_argument("-j", "--jsondump", help="Filepath/name for input JSON files")
+parser.add_argument("-i", "--img", help="Filepath/name prefix for the output figure")
 args = parser.parse_args()
 
 # Function used to take a general counter list in and plot bar columns
@@ -22,6 +32,7 @@ def display_general_barchart(name, counter_list):
     keys = [x[0] for x in counter_list[:l_size]]
     counters = [x[1] for x in counter_list[:l_size]]
 
+    plt.figure()
     plt.bar(range(l_size), counters)
     plt.xticks(range(l_size), keys)
     if len(counters):
@@ -37,28 +48,8 @@ def display_general_barchart(name, counter_list):
         plt.title("Registers used most frequently as destination registers")
         plt.xlabel("Registers")
         plt.ylabel("Number of times read to")
-    elif name == "imm":
-        plt.title("Most frequent immediate values")
-        plt.xlabel("Immediates")
-        plt.ylabel("Number of times used")
-    elif name == "address_offset":
-        plt.title("Most frequent address offset values")
-        plt.xlabel("Immediates (Offsets)")
-        plt.ylabel("Number of times used")
-    elif name == "branch_offset":
-        plt.title("Most frequent branch offsets values")
-        plt.xlabel("Immediates (Branch offsets)")
-        plt.ylabel("Number of times used")
-    elif name == "shift":
-        plt.title("Most frequent shift values")
-        plt.xlabel("Immediates (Shifts)")
-        plt.ylabel("Number of times used")
-    elif name == "arith":
-        plt.title("Most frequent arithmetic values")
-        plt.xlabel("Immediates (Arithmetic values)")
-        plt.ylabel("Number of times used")
     
-    plt.show()
+    plt.savefig(args.img+"_"+name)
 
 # Function used to see how much each register available in the system was used as a source or destination register
 def reg_distribution_plot(all_regs):
@@ -66,6 +57,7 @@ def reg_distribution_plot(all_regs):
     rs_list = [x[1]["rs"] for x in all_regs]
     rd_list = [x[1]["rd"] for x in all_regs]
 
+    plt.figure()
     plt.bar(range(len(reg_names)), height=rs_list, width=0.4, align='edge', label="rs")
     plt.bar(range(len(reg_names)), height=rd_list, width=-0.4, align='edge', label="rd")
 
@@ -74,35 +66,17 @@ def reg_distribution_plot(all_regs):
     plt.ylabel("Frequency")
     plt.legend()
 
-    plt.show()
-
-# Visualise the distribution of the immediates and what they are used for
-def imm_distribution_plot(off, branch_off, shift, arith):
-    sums_list = [0]*4
-    sums_list[0] = sum(i for _,i in off)
-    sums_list[1] = sum(i for _,i in branch_off)
-    sums_list[2] = sum(i for _,i in shift)
-    sums_list[3] = sum(i for _,i in arith)
-    sums_list = sorted(sums_list, reverse=True)
-
-    plt.bar(range(len(sums_list)), sums_list)
-    plt.xticks(range(len(sums_list)), ["Immediate total", "Address offsets", 
-        "Branch offsets", "Shift", "Arithmetic"])
-    plt.title("Immediate distribution of "+str(sum(sums_list))+" immediates")
-    plt.xlabel("Immediate type")
-    plt.ylabel("Frequency")
-    plt.show()
+    plt.savefig(args.img+"_dist")
 
 def main():
     # Read in the associated lists and store in the data dictionary
     with open(args.jsondump, 'r') as dump:
         data = json.load(dump)
 
-    # Visualise distribution of immediates (What the immediates are used for)
-    imm_distribution_plot(data["address_offset"], data["branch_offset"], 
-        data["shift"], data["arith"])
+    # data is the dictionary storing the the registers and their counters
+    #   which determine how often they've been used as a specific register.
 
-    all_regs = list(data["all_regs"].items())
+    all_regs = list(data.items())
     # Visualise the distribution of the registers with what they are used as (rs and rd)
     reg_distribution_plot(all_regs)
 
@@ -115,10 +89,6 @@ def main():
     sorted_rd_list = sorted(rd_list, key=lambda x: x[1], reverse=True)
     # Plot the general bar chart giving the most popular regs used as dest regs
     display_general_barchart("rd", sorted_rd_list)
-
-    # Iterate through the dictionary and plot general bar column graphs for them
-    for k, v in data.items():
-        display_general_barchart(k, v)
 
 if __name__ == "__main__":
     main()
